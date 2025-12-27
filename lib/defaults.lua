386896520904113f9e96d58dc38f13705d80a8b2
Shizo-S <bznznznznlsksjjsjs@gmail.com>
2025-12-28 05:05:47 +0800

    Create defaults.lua

---
+++ lib/defaults.lua
@@ -0,0 +1,14 @@
+--[[
+	Default Configuration
+]]
+
+return {
+	comment = "// IiI1I",
+	variablecomment = "IiIIIIIiIIIIIIIIoOIiiIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+	cryptvarcomment = true,
+	variablename = "QuardFuscator",
+	version = "v1.3.1",
+	silent = false,
+	force = false,
+	openfile = false
+}
